dcd7e7419c3ee1f997bf91e9e70e6dacfb254fed
jonnaliesel <jonnaliesel@gmail.com>
2021-09-20 16:21:43 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,7 @@
+require('ditenv').config({path: "./config.env"})
+
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
