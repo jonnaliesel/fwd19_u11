76ce2e0ef8cb13e21934f4df79b31e0673a38e69
jonnaliesel <jonnaliesel@gmail.com>
2021-09-20 22:02:29 +0200

    add routing

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-require('ditenv').config({path: "./config.env"})
+require('dotenv').config({path: "./config.env"})
 
 const express = require('express')
 const app = express()
+
+app.use(express.json())
+app.use('/api/auth', require('./routes/auth'))
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
