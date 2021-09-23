c8b24be47c0e783df2e7dfd4497c8b766bb0b48d
jonnaliesel <jonnaliesel@gmail.com>
2021-09-23 23:21:31 +0200

    add nodemailer and resetpassword route

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require('../models/User')
 const ErrorResponse = require('../utils/errorResponse')
+const sendEmail = require('../utils/sendEmail')
 
 exports.register = async (req, res, next) => {
   const {username, email, password} = req.body
@@ -47,8 +48,49 @@ exports.login = async (req, res, next) => {
   }
 }
 
-exports.forgotPassword = (req, res, next) => {
-  res.send('Forgot Password Route')
+exports.forgotPassword = async (req, res, next) => {
+  const { email } = req.body
+
+  try {
+    const user = await User.findOne({email})
+
+    if(!user) {
+      return next(new ErrorResponse('Email could not be sent', 404))
+    }
+
+    const resetToken = user.getResetPasswordToken()
+
+    await user.save()
+
+    const resetUrl = `http://localhost:3000/passwordReset/${resetToken}`
+    const message = `
+      <h1>Du ville återskapa ditt lösenord</h1>
+      <p>Följ länken för att skapa ett nytt lösenord</p>
+      <a href="${resetUrl}" clicktracking=off>${resetUrl}</a> 
+    `
+
+    try {
+      await sendEmail({
+        to: user.email,
+        subject: 'Återställ lösenord',
+        text: message
+      })
+
+      res.status(200).json({
+        success: true, 
+        data: 'Email sent'
+      })
+    } catch (error) {
+      user.getResetPasswordToken = undefined
+      user.getResetPasswordExpire = undefined
+
+      await user.save()
+
+      return next(new ErrorResponse('Email could not be sent', 500))
+    }
+  } catch (error) {
+    return next(error)
+  }
 }
 
 exports.resetPassword = (req, res, next) => {
