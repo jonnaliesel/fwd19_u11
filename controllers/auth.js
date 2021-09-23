c3ec32918b52f049b2db7c5f65d5d9884eb984f0
jonnaliesel <jonnaliesel@gmail.com>
2021-09-23 18:21:49 +0200

    add login function and password matching

--- controllers/auth.js
+++ controllers/auth.js
@@ -22,8 +22,45 @@ exports.register = async (req, res, next) => {
   }
 }
 
-exports.login = (req, res, next) => {
-  res.send('Login Route')
+exports.login = async (req, res, next) => {
+  const {email, password} = req.body
+
+  if(!email || !password) {
+    res.status(400).json({
+      success: false,
+      error: 'Vänligen ange mejladress och lösenord'
+    })
+  }
+
+  try {
+    const user = await User.findOne({ email }).select('+password')
+    if(!user) {
+      res.status(404).json({
+        success: false, 
+        error: 'Ogiltiga uppgifter'
+      })
+    }
+
+    const isMatch = await user.matchPassword(password)
+
+    if (!isMatch) {
+      res.status(404).json({
+        success: false, 
+        error: 'Ogiltiga uppgifter'
+      })
+    }
+
+    res.status(200).json({
+      success: true,
+      token: 'bcducgi8w7cwiucv78i'
+    })
+
+  } catch (error) {
+    res.status(500).json({
+      success: false, 
+      error: error.message
+    })
+  }
 }
 
 exports.forgotPassword = (req, res, next) => {
