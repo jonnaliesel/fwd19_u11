59f7f40ec3a4793bbf9ce60f53ce36c8ccdc4747
jonnaliesel <jonnaliesel@gmail.com>
2021-09-23 21:48:45 +0200

    fix error

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config({ path: './config.env' })
  express = require('express')
 const connectDB = require('./config/db')
+const errorHandler = require('./middleware/error')
 const app = express()
 
 // Connect DB
@@ -8,6 +9,9 @@ connectDB()
 
 app.use(express.json())
 app.use('/api/auth', require('./routes/auth'))
+
+// Error Handler
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
