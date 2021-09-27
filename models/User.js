525818b1c95ef7accb10a888411ad1c98202f470
jonnaliesel <jonnaliesel@gmail.com>
2021-09-27 11:15:30 +0200

    Remove username from user schema

--- models/User.js
+++ models/User.js
@@ -4,10 +4,6 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
 const UserSchema = new mongoose.Schema({
-  username: {
-    type: String, 
-    required: [true, 'Vänligen ange ett användarnamn']
-  },
   email: {
     type: String,
     required: [true, 'Vänligen ange en mejladress'],
