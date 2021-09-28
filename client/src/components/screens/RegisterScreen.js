deb844c9f4b9bc2c7c15062f708ebb6ba7c250fe
jonnaliesel <jonnaliesel@gmail.com>
2021-09-28 16:24:02 +0200

    add register screen

---
+++ client/src/components/screens/RegisterScreen.js
@@ -0,0 +1,92 @@
+import { useState } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+
+const RegisterScreen = ({ history }) => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const [error, setError] = useState('')
+
+    const registerHandler = async (e) => {
+        e.preventDefault()
+
+        const config = {
+            header: {
+                "Content-Type": "application/json"
+            }
+        }
+        
+        if(password !== confirmPassword) {
+            setPassword('')
+            setConfirmPassword('')
+            setTimeout(() => {
+                setError('')
+            }, 5000)
+            return setError('Passwords do not match')
+        }
+
+        try {
+            const { data } = await axios.post('/api/auth/register', {email, password},
+            config)
+
+            localStorage.setItem('authToken', data.token)
+
+            history.push('/')
+        } catch (error) {
+            setError(err.response.data.error)
+            setTimeout(() => {
+                setError('')
+            }, 5000)
+        }
+    }
+
+    return (
+        <div className="">
+            <form onSubmit={registerHandler} className="">
+                <h3 className="">Register</h3>
+                {error && <span className="text-red-800">{error}</span>}
+                <div className="">
+                    <label htmlFor="email">Email:</label>
+                    <input 
+                        type="email"
+                        required
+                        id="email"
+                        placeholder="Enter email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+                
+                <div className="">
+                    <label htmlFor="password">Password:</label>
+                    <input 
+                        type="password"
+                        required
+                        id="password"
+                        placeholder="Enter password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+
+                <div className="">
+                    <label htmlFor="confirmpassword">Confirm password:</label>
+                    <input 
+                        type="password"
+                        required
+                        id="confirmpassword"
+                        placeholder="Confirm password"
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                    />
+                </div>
+
+                <button type="submit" className="btn-primary">Register</button>
+                <span className="">Already have an account? <Link to="/login">Login</Link></span>
+            </form>
+        </div>
+    )
+}
+
+export default RegisterScreen
